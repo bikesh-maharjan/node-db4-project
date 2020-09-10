fdf74e54b0818d38392c564f45653a2e0e7584e1
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-09-10 15:33:12 -0700

    migrations table created now moving on to Seed

--- migrations/20200910152418_create_new_recipes.js
+++ migrations/20200910152418_create_new_recipes.js
@@ -1,8 +1,45 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments();
+      tbl.string("recipe", 255).notNullable().index().unique();
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments();
+      tbl.string("ingredient_name", 255).notNullable().index().unique();
+    })
+    .createTable("recipe_ingredients", (tbl) => {
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onDelete("RESTRICT") // Restrict, CASCADE, SET Null, Do Nothing
+        .onUpdate("CASCADE");
 
-exports.up = function(knex) {
-  
+      tbl.integer("quantity").unsigned().notNullable();
+    })
+    .createTable("steps", (tbl) => {
+      tbl.increments();
+      tbl.integer("step_number").unsigned().notNullable();
+
+      tbl.string("instructions", 255).notNullable();
+
+      tbl
+        .integer("recipe_id")
+        .notNullable()
+        .references("id")
+        .inTable("receipes")
+        .onDelete("RESTRICT") // Restrict, CASCADE, SET Null, Do Nothing
+        .onUpdate("CASCADE");
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("steps")
+    .dropTableIfExists("recipe_ingredients")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
 };
